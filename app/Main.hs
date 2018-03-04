840836f4420d41364b0285e64e9ef0d36a231a49
Paul <paulll.lucas@gmail.com>
2018-03-03 22:25:20 -0800

    Handle IO input

--- app/Main.hs
+++ app/Main.hs
@@ -5,19 +5,28 @@ import           Data.List.Split
 import           Graphics.GD
 import           System.Environment
 
+throw :: a
+throw = errorWithoutStackTrace "Provide either a PNG or JPG file"
+
+sortImage :: Image -> IO Image
 sortImage image = do
   (width, height) <- imageSize image
   let points = [(x, y) | x <- [0 .. width], y <- [0 .. height]]
       colors = mapM (`getPixel` image) points
       render = mapM (\(point, color) -> setPixel point color image)
-  render . zip points . sort =<< colors
+  _ <- render . zip points . sort =<< colors
   return image
 
+pixelSort :: [String] -> IO ()
 pixelSort [name, "png"] =
   loadPngFile (name ++ ".png") >>= sortImage >>= savePngFile "out.png"
 pixelSort [name, "jpeg"] =
   loadJpegFile (name ++ ".jpeg") >>= sortImage >>= saveJpegFile 95 "out.jpeg"
+pixelSort _ = throw
 
+main :: IO ()
 main = do
-  [filename] <- getArgs
-  pixelSort $ splitOn "." filename
+  args <- getArgs
+  case args of
+    (filename:_) -> pixelSort $ splitOn "." filename
+    _            -> throw
