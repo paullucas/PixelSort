88762aefe13bce5eac406dac0628f78f60c0c6ff
Paul <paulll.lucas@gmail.com>
2018-03-03 17:18:11 -0800

    Reformat

--- app/Main.hs
+++ app/Main.hs
@@ -1,27 +1,22 @@
 module Main where
 
-import Data.List
-import Data.List.Split
-import System.Environment
-import Graphics.GD
+import           Data.List
+import           Data.List.Split
+import           Graphics.GD
+import           System.Environment
 
 sortImage image = do
-    (width, height) <- imageSize image
-    let points = [(x,y) | x <- [0..width], y <- [0..height]]
-        colors = mapM (`getPixel` image) points
-        render = mapM (\(point,color) -> setPixel point color image)
-    render . zip points . sort =<< colors
-    return image
+  (width, height) <- imageSize image
+  let points = [(x, y) | x <- [0 .. width], y <- [0 .. height]]
+      colors = mapM (`getPixel` image) points
+      render = mapM (\(point, color) -> setPixel point color image)
+  render . zip points . sort =<< colors
+  return image
 
 pixelSort [name, "png"] =
-  loadPngFile (name ++ ".png")
-  >>= sortImage
-  >>= savePngFile "out.png"
-
+  loadPngFile (name ++ ".png") >>= sortImage >>= savePngFile "out.png"
 pixelSort [name, "jpeg"] =
-  loadJpegFile (name ++ ".jpeg")
-  >>= sortImage
-  >>= saveJpegFile 95 "out.jpeg"
+  loadJpegFile (name ++ ".jpeg") >>= sortImage >>= saveJpegFile 95 "out.jpeg"
 
 main = do
   [filename] <- getArgs
